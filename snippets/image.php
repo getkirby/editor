97c47d429a0743a96c22974a01ef6f1c5f02e375
Bastian Allgeier <mail@bastianallgeier.com>
2019-08-02 10:35:21 +0200

    Add links to images

--- snippets/image.php
+++ snippets/image.php
@@ -1,8 +1,15 @@
 <figure>
+  <?php if ($attrs->link()->isNotEmpty()): ?>
+  <a href="<?= $attrs->link()->toUrl() ?>">
     <img src="<?= $attrs->src() ?>" alt="<?= $attrs->alt() ?>">
-    <?php if ($attrs->caption()->isNotEmpty()): ?>
-    <figcaption>
-        <?= $attrs->caption() ?>
-    </figcaption>
-    <?php endif ?>
+  </a>
+  <?php else: ?>
+  <img src="<?= $attrs->src() ?>" alt="<?= $attrs->alt() ?>">
+  <?php endif ?>
+
+  <?php if ($attrs->caption()->isNotEmpty()): ?>
+  <figcaption>
+    <?= $attrs->caption() ?>
+  </figcaption>
+  <?php endif ?>
 </figure>
