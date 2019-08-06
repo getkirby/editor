8b0e719fafbdd680911e560b15bf4cb61bdb0231
Bastian Allgeier <mail@bastianallgeier.com>
2019-08-06 16:07:49 +0200

    Properly encode html elements in code blocks

--- snippets/code.php
+++ snippets/code.php
@@ -1,1 +1,1 @@
-<pre><code class="language-<?= $attrs->language()->or('text') ?>"><?= $content ?></code></pre>
+<pre><code class="language-<?= $attrs->language()->or('text') ?>"><?= $content->html(false) ?></code></pre>
