f2f8a7f1f7cc6582f13f51cb2d5ecfd8d2ef25d2
Bastian Allgeier <mail@bastianallgeier.com>
2019-08-19 18:46:21 +0200

    Adjust code style

--- snippets/ul.php
+++ snippets/ul.php
@@ -1,7 +1,7 @@
-<?php if (is_null($prev) || $prev->type() !== 'ul'): ?>
+<?php if ($prev === null || $prev->type() !== 'ul'): ?>
 <ul>
 <?php endif ?>
 <li><?= $content ?></li>
-<?php if (is_null($next) || $next->type() !== 'ul'): ?>
+<?php if ($next === null || $next->type() !== 'ul'): ?>
 </ul>
 <?php endif ?>
