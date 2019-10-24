f74ae5d00a18227e3663d2131f266fc72e1feefd
Bastian Allgeier <mail@bastianallgeier.com>
2019-10-24 18:10:07 +0200

    Require Kirby as dependency and fix test bootstrapper

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,12 @@ ini_set('memory_limit', '512M');
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
-require_once dirname(__DIR__, 4) . '/kirby/bootstrap.php';
+require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
+
+$bootstrapper = dirname(__DIR__, 4) . '/kirby/bootstrap.php';
+
+if (is_file($bootstrapper)) {
+    require_once $bootstrapper;
+}
 
 kirby();
