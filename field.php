5656aefb30f4be7ab50fa08b62c431fa031cc0c6
Bastian Allgeier <mail@bastianallgeier.com>
2019-11-05 12:57:28 +0100

    Require Kirby 3.3.0

--- field.php
+++ field.php
@@ -41,7 +41,7 @@ return [
     ],
     'computed' => [
         'isSupported' => function () {
-            if (version_compare($this->kirby()->version(), '3.3.0-rc.5')) {
+            if (version_compare($this->kirby()->version(), '3.3.0')) {
                 throw new Exception('The editor requires Kirby version 3.3.0 or higher');
             }
         },
