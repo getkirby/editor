b4e0eeb788c2ff8389da0278fbbba5f26f061ee0
Simon Perdrisat <perdrisat@gmail.com>
2020-07-15 11:06:40 +0200

    Use the image object in snippets

--- snippets/image.php
+++ snippets/image.php
@@ -1,11 +1,11 @@
 <?php if ($block->isNotEmpty()): ?>
 <figure<?= attr(['class' => $attrs->css()->value()], ' ') ?>>
   <?php if ($attrs->link()->isNotEmpty()): ?>
-  <a href="<?= $attrs->link()->toUrl() ?>">
-    <img src="<?= $src ?>" alt="<?= $attrs->alt() ?>">
-  </a>
+    <a href="<?= $attrs->link()->toUrl() ?>">
+      <?= $image ?>
+    </a>
   <?php else: ?>
-  <img src="<?= $src ?>" alt="<?= $attrs->alt() ?>">
+    <?= $image ?>
   <?php endif ?>
 
   <?php if ($attrs->caption()->isNotEmpty()): ?>
