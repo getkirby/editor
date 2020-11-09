168f26c86285b3005f6ed75d2699fb5c22733318
PatricB <lspeck@gmx.de>
2020-11-09 03:05:50 +0100

    make block position definable in the list of blocks #185
    Signed-off-by: PatricB <lspeck@gmx.de>

--- src/components/Plugins.js
+++ src/components/Plugins.js
@@ -1,10 +1,11 @@
 window.editor = {
-  blocks: {},
-  block(type, params) {
+  blocks: [],
+  block(type, params, position) {
     const defaults = {
       type: type,
       icon: "page",
     };
+    const positionMatch = /(after|before):(\S+)/.exec(position);
 
     // extend the params with the defaults
     params = {
@@ -21,6 +22,24 @@ window.editor = {
       placeholder: params.placeholder,
     };
 
-    this.blocks[type] = params;
+    // add block to blocks array
+    if (position && positionMatch !== null && this.blocks.findIndex(b => b[0] === positionMatch[2]) !== -1) {
+      // add after/before a given type if position parameter is given and sibling block isset
+      // find position of siblings type
+      let siblingsIndex = this.blocks.findIndex(b => b[0] === positionMatch[2]);
+
+      // add new block before/after sibling
+      switch (positionMatch[1]) {
+        case 'before':
+          this.blocks.splice(siblingsIndex, 0, [type,params])
+          break;
+        case 'after':
+        default:
+          this.blocks.splice(siblingsIndex + 1, 0, [type,params])
+      }
+    } else {
+      // add block add the end of blocks
+      this.blocks.push([type, params]);
+    }
   }
 };
